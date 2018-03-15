ee1db50fa478ae187d40856a1fad73e40a33f8a6
Don Biven <biven.don@gmail.com>
2018-03-14 23:37:19 -0500

    Working on display of black keys

--- key.js
+++ key.js
@@ -1,4 +1,4 @@
-function Pkey( inX, w, h, color, note, freq, wl) {
+function Pkey( inX, w, h, color, note, freq, wl, whiteWidth, blackWidth) {
   this.x = inX;
   this.w = w;
   this.x2 = this.x + this.w;
@@ -8,75 +8,107 @@ function Pkey( inX, w, h, color, note, freq, wl) {
   this.waveLength = wl;
   this.fillColor = color;
   this.isClicked = false;
-  //this.shape = 'left';
-  var startNote = this.note.substr(0,1)
-  switch(startNote) {
-    case 'C':
-      this.pos = 'left';
-      break;
-    case 'D':
-      this.pos = 'center';
-      break;
-    case 'E':
-      this.pos = 'right';
-      break;
-    case 'F':
-      this.pos = 'left';
-      break;
-    case 'G':
-      this.pos = 'center';
-      break;
-    case 'A':
-      this.pos = 'center';
-      break;
-    case 'B':
-      this.pos = 'right';
-      break;
+  this.whiteWidth = whiteWidth;
+  this.blackWidth = blackWidth;
+  this.blackHeight = floor(this.h * .60);
+  this.octave = this.note.substr(1,1);
+  if (this.octave == '#') {
+    this.octave = this.note.substr(2,1);
+    this.note = this.note.substr(0,2);
+  } else {
+    this.note = this.note.substr(0,1);
+  }
+
+  if (this.fillColor == 255) {
+    switch(this.note) {
+      case 'C':
+        this.pos = 'left';
+        break;
+      case 'D':
+        this.pos = 'center';
+        break;
+      case 'E':
+        this.pos = 'right';
+        break;
+      case 'F':
+        this.pos = 'left';
+        break;
+      case 'G':
+        this.pos = 'center';
+        break;
+      case 'A':
+        this.pos = 'center';
+        break;
+      case 'B':
+        this.pos = 'right';
+        break;
+    }
+  } else {
+    switch(this.note) {
+      case 'F#':
+        this.pos = 'skip';
+        this.x = this.x + (this.whiteWidth) - (this.blackWidth / 2);
+        break;
+      case 'C#':
+        this.pos = 'skip';
+        this.x = this.x + (this.whiteWidth) - (this.blackWidth / 2);
+        break;
+      default:
+        this.pos = 'noskip';
+        this.x = this.x - (this.blackWidth / 2);
+        break;
+    }
   }
 
   this.show = function() {
     stroke(0);
     strokeWeight(2);
     fill(this.fillColor);
-    switch(this.pos) {
-      case 'left':
-        beginShape();
-        vertex(this.x, 0);
-        vertex(this.x2 - 20, 0);
-        vertex(this.x2 - 20, this.h/2);
-        vertex(this.x2, this.h/2);
-        vertex(this.x2, this.h);
-        vertex(this.x, this.h);
-        endShape(CLOSE);
-        break;
-      case 'center':
-        beginShape();
-        vertex(this.x + 20, 0);
-        vertex(this.x2 - 20, 0);
-        vertex(this.x2 - 20, this.h/2);
-        vertex(this.x2, this.h/2);
-        vertex(this.x2, this.h);
-        vertex(this.x, this.h);
-        vertex(this.x, this.h/2);
-        vertex(this.x + 20, this.h/2);
-        endShape(CLOSE);
-        break;
-      case 'right':
-        beginShape();
-        vertex(this.x + 20, 0);
-        vertex(this.x2, 0);
-        vertex(this.x2, this.h);
-        vertex(this.x, this.h);
-        vertex(this.x, this.h/2);
-        vertex(this.x + 20, this.h/2);
-        endShape(CLOSE);
-        break;
+    if ( this.fillColor == 255) { //drqw white keys
+      switch(this.pos) {
+        case 'left':
+          beginShape();
+          vertex(this.x, 0);
+          vertex(this.x2 - this.blackWidth/2, 0);
+          vertex(this.x2 - this.blackWidth/2, this.blackHeight);
+          vertex(this.x2, this.blackHeight);
+          vertex(this.x2, this.h);
+          vertex(this.x, this.h);
+          endShape(CLOSE);
+          break;
+        case 'center':
+          beginShape();
+          vertex(this.x + this.blackWidth/2, 0);
+          vertex(this.x2 - this.blackWidth/2, 0);
+          vertex(this.x2 - this.blackWidth/2, this.blackHeight);
+          vertex(this.x2, this.blackHeight);
+          vertex(this.x2, this.h);
+          vertex(this.x, this.h);
+          vertex(this.x, this.blackHeight);
+          vertex(this.x + this.blackWidth/2, this.blackHeight);
+          endShape(CLOSE);
+          break;
+        case 'right':
+          beginShape();
+          vertex(this.x + this.blackWidth/2, 0);
+          vertex(this.x2, 0);
+          vertex(this.x2, this.h);
+          vertex(this.x, this.h);
+          vertex(this.x, this.blackHeight);
+          vertex(this.x + this.blackWidth/2, this.blackHeight);
+          endShape(CLOSE);
+          break;
+      }
+    } else { //draw black keys
+      //if (this.pos !== 'skip') {
+        rect(this.x, 0, this.blackWidth, this.blackHeight);
+      //}
     }
     //rect(this.x, 0, this.w, this.h);
   }
 
   this.clicked = function(x,y) {
-    if(x > inX && x < (inX + w) && y > w && y < (w + h)) {
+    if(x > this.x && x < (this.x + this.w) && y > this.w && y < (this.w + this.h)) {
       return true;
     } else {
       return false;
--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var pX, pY, xPressed, yPressed, keyNote;
 var scl = 50;
+var blackWidth = 30;
 var fequency = {};
 var keys = [];
 
@@ -10,11 +11,12 @@ function setup() {
   xPressed = createP();
   yPressed = createP();
   keyNote = createP();
+  octave = createP();
   keys = createKeys();
 }
 
 function draw() {
-  background(0);
+  background(255);
   for (var i = 0; i < keys.length; i++){
     keys[i].show();
   }
@@ -26,6 +28,7 @@ function mousePressed() {
   for(i=0; i < keys.length; i++) {
     if(keys[i].clicked(mouseX, mouseY)) {
       keyNote.html(keys[i].note);
+      octave.html(keys[i].octave);
     }
   }
 }
@@ -36,9 +39,19 @@ function createKeys(){
   var keyCount = width / scl;
   var topLeft = 0;
   for (var i = 0; i < keyCount; i++) {
-    var pianoKey = new Pkey(topLeft,scl,height,255,wFreq[i].note,wFreq[i].f, wFreq[i].wavelength);
+    var pianoKey = new Pkey(topLeft,scl,height,255,wFreq[i].note,wFreq[i].f, wFreq[i].wavelength, scl, blackWidth);
     k.push(pianoKey);
     topLeft += scl;
+  }
+  topLeft = 0;
+  for (i = 0; i < keyCount / 2; i++) {
+    var pianoKey = new Pkey(topLeft, blackWidth, height, 0, bFreq[i].note, bFreq[i].f, bFreq[i].wavelength, scl, blackWidth);
+    k.push(pianoKey);
+    if (pianoKey.pos == 'skip') {
+      topLeft += (scl * 2);
+    } else {
+      topLeft += scl;
+    }
   }
   return k;
 }
