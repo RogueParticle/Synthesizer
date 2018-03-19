bb8eb74d68c315772e2d02398ad1c84705da92ba
RogueParticle <biven.don@gmail.com>
2018-03-18 23:22:49 -0500

    Created and configured octave window. Added clicked function.

--- octavewindow.js
+++ octavewindow.js
@@ -1,8 +1,47 @@
-function OctaveWindow(level) {
-  var pX, pY, xPressed, yPressed, keyNote, octaveP, octNote, octPerWindow, startOctave;
+function OctaveWindow() {
+  this.createOctaves = function() {
+    var o = [];
+    var level = 0;
+    for (i = 0; i < this.octCount; i++){
+      var oct = new Octave(level);
+      o.push(oct);
+      level++;
+    }
+    return o;
+  }
+
+  this.show = function() {
+    //var offset = this.startOctave;
+    var offset = 0;
+    for (var currentOctave = this.startOctave; currentOctave < this.startOctave + this.octPerWindow; currentOctave++){
+      this.octaves[currentOctave].show(offset);
+      offset += this.octaveWidth;
+    }
+  }
+
+  this.clicked = function(x,y) {
+    var found = false;
+    var offset = 0;
+    for(i=this.startOctave; i < this.startOctave + this.octPerWindow; i++) {
+      var octNote = this.octaves[i].clicked(mouseX, mouseY, offset);
+      if(octNote !== 0) {
+        found = true;
+        break;
+      } else {
+        found = false;
+      }
+      offset += this.octaveWidth;
+    }
+    if (found) return octNote;
+  }
+
   var whiteWidth = 50;
   var blackWidth = 30;
-  var fequency = {};
-  var octaves = [];
-  var octaveWidth = 350;
+  this.octaves = [];
+  this.octaveWidth = 350;
+  this.octPerWindow = 3;
+  this.startOctave = 3;
+  this.octCount = 8;
+
+  this.octaves = this.createOctaves();
 }
--- sketch.js
+++ sketch.js
@@ -1,67 +1,31 @@
-var pX, pY, xPressed, yPressed, keyNote, octaveP, octNote, octPerWindow, startOctave;
-var whiteWidth = 50;
-var blackWidth = 30;
-var fequency = {};
-var octaves = [];
-var octaveWidth = 350;
-var octaveWindow = [];
+var pX, pY, xPressed, yPressed, octaveP, octNoteP;
+var octWindow = new OctaveWindow();
 
 function setup() {
-  createCanvas(octaveWidth * 3, 200);
-  //var octWindow = new OctaveWindow();
+  createCanvas(octWindow.octaveWidth * octWindow.octPerWindow, 200);
   xPressed = createP('none');
   yPressed = createP('none');
   octaveP = createP('none');
   octNoteP = createP('none');
   octFreqP = createP('none');
-  octaves = createOctaves();
-  octPerWindow = 3;
-  startOctave = 0;
-  octaveWindow.push(octaves[0])
-  octaveWindow.push(octaves[1]);
-  octaveWindow.push(octaves[2]);
 }
 
 function draw() {
   background(85);
-  var offset = 0;
-  for (var currentOctave = startOctave; currentOctave < startOctave + octPerWindow; currentOctave++){
-    octaves[currentOctave].show(offset);
-    offset += octaveWidth;
-  }
+  octWindow.show();
 }
 
 function mousePressed() {
-  var offset = 0;
   xPressed.html(mouseX);
   yPressed.html(mouseY);
-  var found = false;
-  for(i=0; i < octaves.length; i++) {
-    var octNote = octaves[i].clicked(mouseX, mouseY, offset)
-    if(octNote !== 0) {
-      octNoteP.html(octNote.note);
-      octaveP.html(octNote.octave);
-      octFreqP.html(octNote.f);
-      found = true;
-      break;
-    } else {
-      octNoteP.html('none');
-      octaveP.html('none');
-      octFreqP.html('none');
-    }
-    if (found) break;
-    offset += octaveWidth;
+  var octNote = octWindow.clicked(mouseX, mouseY);
+  if(octNote !== 0) {
+    octNoteP.html(octNote.note);
+    octaveP.html(octNote.octave);
+    octFreqP.html(octNote.f);
+  } else {
+    octNoteP.html('none');
+    octaveP.html('none');
+    octFreqP.html('none');
   }
-}
-
-function createOctaves(){
-  //octaves contain keys
-  var o = [];
-  var level = 0;
-  for (i = 0; i < 6; i++){
-    var oct = new Octave(level);
-    o.push(oct);
-    level++;
-  }
-  return o;
 }
